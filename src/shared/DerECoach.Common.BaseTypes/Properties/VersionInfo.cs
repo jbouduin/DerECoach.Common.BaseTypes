c81d5bc72046ad32e97b0eaa990023f4d1731c99
Johan Bouduin <johan@der-e-coach.de>
2018-10-03 11:27:34 +0200

    to create a commit

--- src/shared/DerECoach.Common.BaseTypes/Properties/VersionInfo.cs
+++ src/shared/DerECoach.Common.BaseTypes/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.1.3")]
-[assembly: AssemblyFileVersion("0.0.1.3")]
+[assembly: AssemblyVersion("...")]
+[assembly: AssemblyFileVersion("...")]
